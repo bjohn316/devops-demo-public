a927167dc1001e3515d63a541a6334c8f2b9b17b
Doc <not@home.com>
2025-08-11 22:00:29 -0500

    initial test

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from main import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+
+def test_hello():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.data == b"Hello, World!"
+    assert response.status_code == 200
